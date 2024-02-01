bb029e6eecc0a8e350ea5689ec236f6e75b60a35
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-02-01 11:30:46 +0100

    add BookingDto according to UML

---
+++ Bookkeeper_API/Data/BookingDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookkeeper_API.Data
+{
+    public class BookingDto
+    {
+        public string? BookingNote { get; set; }
+
+        required public int DebitAccount { get; set; }
+
+        required public int CreditAccount { get; set; }
+
+        required public decimal Amount { get; set; }
+    }
+}
