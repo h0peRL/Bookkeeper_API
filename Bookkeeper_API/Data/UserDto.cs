218d40d5471ffa34ac46ffcead13ac6cb0a79c08
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-02-01 11:28:02 +0100

    add UserDto according to UML

---
+++ Bookkeeper_API/Data/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookkeeper_API.Data
+{
+    public class UserDto
+    {
+        required public string Username { get; set; }
+
+        required public string Password { get; set; }
+    }
+}
