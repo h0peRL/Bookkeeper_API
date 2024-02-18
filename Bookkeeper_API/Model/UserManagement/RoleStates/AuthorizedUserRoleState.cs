5839b94c2f0104de29e4ae22a5290a461f177543
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-02-18 20:02:06 +0100

    create user role state classes

---
+++ Bookkeeper_API/Model/UserManagement/RoleStates/AuthorizedUserRoleState.cs
@@ -0,0 +1,17 @@
+﻿namespace Bookkeeper_API.Model.UserManagement.RoleStates
+{
+    public class AuthorizedUserRoleState : IUserRoleState
+    {
+        private static readonly string _roleName = "authorized user";
+
+        public string GetRoleName()
+        {
+            return _roleName;
+        }
+
+        public void SetRoleState(IUserRoleState roleState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bookkeeper_API/Model/UserManagement/RoleStates/NewUserRoleState.cs
@@ -0,0 +1,17 @@
+﻿namespace Bookkeeper_API.Model.UserManagement.RoleStates
+{
+    public class NewUserRoleState : IUserRoleState
+    {
+        private static readonly string _roleName = "new user";
+
+        public string GetRoleName()
+        {
+            return _roleName;
+        }
+
+        public void SetRoleState(IUserRoleState roleState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
