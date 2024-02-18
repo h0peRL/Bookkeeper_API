17977b136850a722ad638fbb207840941a2241e7
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-02-18 20:08:40 +0100

    create auth controller

---
+++ Bookkeeper_API/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Bookkeeper_API.Data.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookkeeper_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
