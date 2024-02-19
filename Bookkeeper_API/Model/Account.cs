e255035ef2573c7025ab82b84d4aa5d4fb5d9d50
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-02-19 16:53:07 +0100

    readjust the Account class to work with the EF

--- Bookkeeper_API/Model/Account.cs
+++ Bookkeeper_API/Model/Account.cs
@@ -2,24 +2,25 @@
 {
     public abstract class Account
     {
-        private readonly int _id;
-        private readonly string _name;
-
         protected Account(int id, string name)
         {
-            _id = id;
-            _name = name;
+            Id = id;
+            Name = name;
         }
 
-        public int Id
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Account"/> class.
+        /// The Entity Framework requires an empty constructor. Please do not remove it.
+        /// </summary>
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        private Account()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
-            get { return _id; }
         }
 
-        public string Name
-        {
-            get { return _name; }
-        }
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
 
         public decimal CalculateBalance()
         {
