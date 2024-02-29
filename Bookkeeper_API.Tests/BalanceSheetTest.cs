6e6bf5c75d24778d8a3e66dc2180055311aa1b74
Ellis Arn <ellisarn@gmail.com>
2024-02-29 08:54:43 +0100

    Implement unittests for balance sheet

--- Bookkeeper_API.Tests/BalanceSheetTest.cs
+++ Bookkeeper_API.Tests/BalanceSheetTest.cs
@@ -1,7 +1,65 @@
-﻿namespace Bookkeeper_API.Tests
+﻿using Bookkeeper_API.Data.DTOs;
+
+namespace Bookkeeper_API.Tests
 {
     public class BalanceSheetTest
     {
+        [Fact(Skip = "not implemented")]
+        public void TestStateBalanceAccounts()
+        {
+            // Arrange
+            BalanceSheet balanceSheet = new();
+            BalanceSheetDto mockBalanceSheet = CreateMockBalanceSheet();
 
+            // Act
+            BalanceSheetDto actualBalanceSheet = balanceSheet.StateBalance();
+
+            // Assert
+            Assert.Equal(mockBalanceSheet.Accounts, actualBalanceSheet.Accounts);
+        }
+
+        [Fact(Skip = "not implemented")]
+        public void TestStateBalanceTotal()
+        {
+            // Arrange
+            BalanceSheet balanceSheet = new();
+            BalanceSheetDto mockBalanceSheet = CreateMockBalanceSheet();
+
+            // Act
+            BalanceSheetDto actualBalanceSheet = balanceSheet.StateBalance();
+
+            // Assert
+            Assert.Equal(mockBalanceSheet.Total, actualBalanceSheet.Total);
+        }
+
+        [Fact(Skip = "not implemented")]
+        public void TestStateBalanceDate()
+        {
+            // Arrange
+            BalanceSheet balanceSheet = new();
+            BalanceSheetDto mockBalanceSheet = CreateMockBalanceSheet();
+
+            // Act
+            BalanceSheetDto actualBalanceSheet = balanceSheet.StateBalance();
+
+            // Assert
+            Assert.InRange(actualBalanceSheet.Date, mockBalanceSheet.Date - 1, mockBalanceSheet.Date + 1);
+        }
+
+        private BalanceSheetDto CreateMockBalanceSheet()
+        {
+            BalanceSheetDto mockBalanceSheet = new()
+            {
+                Accounts = new Dictionary<(int, string), decimal>
+                {
+                    { (1020, "Bank"), 100 },
+                    { (2000, "VLL"), 200 },
+                    { (1100, "FLL"), 300 }
+                },
+                Total = 400,
+                Date = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds
+            };
+            return mockBalanceSheet;
+        }
     }
 }
