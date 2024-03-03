13be83d9cfdd995a68ef95d8129a367926a232f0
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-03-03 17:11:38 +0100

    add new data repo methods to concrete implementations

--- Bookkeeper_API.Tests/TestObjectClasses/TestDataRepository.cs
+++ Bookkeeper_API.Tests/TestObjectClasses/TestDataRepository.cs
@@ -4,7 +4,42 @@ namespace Bookkeeper_API.Tests.TestObjectClasses
 {
     internal class TestDataRepository : IDataRepository
     {
+        public void AddBookingRecord(BookingRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AuthorizeNewUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisapproveExistingUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<BookingRecord> FindBookingRecordsForAccount(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Account> GetBalanceSheetAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Account> GetIncomeStatementAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int userId)
         {
             throw new NotImplementedException();
         }
--- Bookkeeper_API/Data/EFCoreDataRepository.cs
+++ Bookkeeper_API/Data/EFCoreDataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Bookkeeper_API.Model;
+using Bookkeeper_API.Model.UserManagement;
 
 namespace Bookkeeper_API.Data
 {
@@ -11,10 +12,45 @@ namespace Bookkeeper_API.Data
             _db = dbContext;
         }
 
+        public void AddBookingRecord(BookingRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AuthorizeNewUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisapproveExistingUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<BookingRecord> FindBookingRecordsForAccount(int accountId)
         {
             return _db.Bookings.Where(b => b.DebitAccount.Id == accountId
             || b.CreditAccount.Id == accountId);
+        }
+
+        public IEnumerable<Account> GetBalanceSheetAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Account> GetIncomeStatementAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int userId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
