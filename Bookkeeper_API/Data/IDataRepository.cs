f683d3afa2c82362b4b7b6b3bb0bee8123fb47f3
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-03-03 16:56:15 +0100

    add operations to data repo interface

--- Bookkeeper_API/Data/IDataRepository.cs
+++ Bookkeeper_API/Data/IDataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Bookkeeper_API.Model;
+using Bookkeeper_API.Model.UserManagement;
 
 namespace Bookkeeper_API.Data
 {
@@ -11,11 +12,60 @@ namespace Bookkeeper_API.Data
     /// </summary>
     public interface IDataRepository
     {
+        // READ OPERATIONS
+
         /// <summary>
         /// Select all booking records related to an account.
         /// </summary>
         /// <param name="accountId">The ID of the account you'd like to look for in the records.</param>
         /// <returns>Collection of booking record objects.</returns>
         IEnumerable<BookingRecord> FindBookingRecordsForAccount(int accountId);
+
+        /// <summary>
+        /// Select all balance sheet accounts.
+        /// </summary>
+        /// <returns>Collection of active and passive accounts.</returns>
+        IEnumerable<Account> GetBalanceSheetAccounts();
+
+        /// <summary>
+        /// Select all income statement accounts.
+        /// </summary>
+        /// <returns>Collection of income and expense accounts.</returns>
+        IEnumerable<Account> GetIncomeStatementAccounts();
+
+        /// <summary>
+        /// Select a user by its Id.
+        /// </summary>
+        /// <param name="userId">Id of the user you are looking for.</param>
+        /// <returns>User with the Id that has been passed as a parameter.</returns>
+        User GetUserById(int userId);
+
+        // INSERT OPERATIONS
+
+        /// <summary>
+        /// Insert a new booking record.
+        /// </summary>
+        /// <param name="record">Record to insert.</param>
+        void AddBookingRecord(BookingRecord record);
+
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        /// <param name="user">New user to be registered. (without an Id).</param>
+        void AddUser(User user);
+
+        // UPDATE OPERATIONS
+
+        /// <summary>
+        /// Authorizes newly registered users to use the system. Without this, the user won't be allowed to perform any operations.
+        /// </summary>
+        /// <param name="userId">Id of the user to be authorized for use.</param>
+        void AuthorizeNewUser(int userId);
+
+        /// <summary>
+        /// Removes authorization from existing users.
+        /// </summary>
+        /// <param name="userId">Id of the user to be disapproved.</param>
+        void DisapproveExistingUser(int userId);
     }
 }
