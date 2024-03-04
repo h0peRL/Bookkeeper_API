54aa6cf5badb3143fccd1a97fa43c7d70b531535
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-03-04 20:22:53 +0100

    create a basic dto converter class

---
+++ Bookkeeper_API/Model/Services/BookingRecordConverter.cs
@@ -0,0 +1,23 @@
+﻿using Bookkeeper_API.Data;
+using Bookkeeper_API.Data.DTOs;
+
+namespace Bookkeeper_API.Model.Services
+{
+    public static class BookingRecordConverter
+    {
+        public static BookingRecord BookingDtoToBookingRecord(BookingDto bookingDto, IDataRepository dataRepository)
+        {
+            Account debitAccount = dataRepository.GetAccountById(bookingDto.DebitAccount);
+            Account creditAccount = dataRepository.GetAccountById(bookingDto.CreditAccount);
+
+            BookingRecord record = new (
+                null,
+                bookingDto.BookingNote,
+                creditAccount,
+                debitAccount,
+                bookingDto.Amount);
+
+            return record;
+        }
+    }
+}
