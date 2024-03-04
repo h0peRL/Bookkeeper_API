ffa3c151bf68e4b80c18f670b842eb8fe6e7572c
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-03-04 21:50:02 +0100

    add simple booking record tests

--- Bookkeeper_API.Tests/BookingRecordTest.cs
+++ Bookkeeper_API.Tests/BookingRecordTest.cs
@@ -1,4 +1,4 @@
-using Bookkeeper_API.Tests.TestObjectClasses;
+﻿using Bookkeeper_API.Tests.TestObjectClasses;
 
 namespace Bookkeeper_API.Tests
 {
@@ -34,6 +34,44 @@ namespace Bookkeeper_API.Tests
             // Assert
             Assert.Equal(100, debitAccount.CalculateBalance());
             Assert.Equal(0, creditAccount.CalculateBalance());
+        }
+
+        public void TestDate()
+        {
+            // Arrange
+            const string bookingNote = "Test";
+            const decimal amount = 100;
+
+            Account debitAccount = _repository.GetAccountById(1020); // Bank
+            Account creditAccount = _repository.GetAccountById(2000); // VLL
+
+            int nowTimestamp = (int)new DateTimeOffset(DateTime.Now.ToUniversalTime()).ToUnixTimeSeconds();
+
+            // Act
+            BookingRecord bookingRecord = new(null, bookingNote, debitAccount, creditAccount, amount);
+            int bookingTimestamp = bookingRecord.BookingDate;
+
+            // Assert
+            Assert.True((bookingTimestamp - nowTimestamp) < 2); // 2s of tolerance
+            Assert.True((bookingTimestamp - nowTimestamp) < -2); // 2s of tolerance
+        }
+
+        public void TestBookingNote()
+        {
+            // Arrange
+            const string expectedNote = "Test";
+            const decimal amount = 100;
+
+            Account debitAccount = _repository.GetAccountById(1020); // Bank
+            Account creditAccount = _repository.GetAccountById(2000); // VLL
+
+            // Act
+            BookingRecord bookingRecord = new(null, expectedNote, debitAccount, creditAccount, amount);
+            string? actualNote = bookingRecord.BookingNote;
+
+            // Assert
+            Assert.NotNull(actualNote);
+            Assert.Equal(expectedNote, actualNote);
         }
     }
 }
