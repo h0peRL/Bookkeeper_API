c20b8dba7d7ea751a4aeb662ceef99bbf9dbdf97
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-03-06 21:04:50 +0100

    implement observer and subject interfaces

---
+++ Bookkeeper_API/Model/BookingProcessor.cs
@@ -0,0 +1,30 @@
+﻿namespace Bookkeeper_API.Model
+{
+    public class BookingProcessor : IObserver
+    {
+        public void Update()
+        {
+            // In this case our subject is expected to be a booking queue.
+            // The booking queue is a singleton, so we do not need a reference to it passed through the params.
+            BookingQueue queue = BookingQueue.GetInstance();
+
+            // The overridden queue is set up to notify us through Update() when a new booking record gets enqueued.
+            // Once we get a notification, this processor should start dequeueing booking requests as long as there is a queue.
+            while (queue.Count > 0)
+            {
+                BookingRecord bookingRecord = queue.Dequeue();
+                try
+                {
+                    bookingRecord.Execute();
+                }
+                catch (Exception)
+                {
+                    throw;
+                    // TODO: Notify the endpoint that there was an issue processing the booking request.
+                }
+            }
+
+            // TODO: Notify the endpoint that the booking request could be processed successfully.
+        }
+    }
+}
--- Bookkeeper_API/Model/BookingQueue.cs
+++ Bookkeeper_API/Model/BookingQueue.cs
@@ -1,5 +1,3 @@
-﻿using System.Runtime.CompilerServices;
-
 namespace Bookkeeper_API.Model
 {
     /// <summary>
@@ -7,7 +5,7 @@ namespace Bookkeeper_API.Model
     /// The point of it is to offer a FIFO-like data structure to process booking requests in an orderly manner.
     /// This way a conflict with impossible bookings can be avoided.
     /// </summary>
-    public class BookingQueue : Queue<BookingRecord>
+    public class BookingQueue : Queue<BookingRecord>, ISubject
     {
         // To implement this class, the following sources have been used:
         // https://refactoring.guru/design-patterns/singleton/csharp/example#example-1
@@ -18,9 +16,15 @@ namespace Bookkeeper_API.Model
         // Singleton instance that gets referenced, if it has been asked for.
         private static BookingQueue? _instance;
 
+        private List<IObserver> _observers;
+
         // Constructor must be private, so only the class itself can use it.
         private BookingQueue()
         {
+            _observers = new List<IObserver>
+            {
+                new BookingProcessor()
+            };
         }
 
         /// <summary>
@@ -42,6 +46,24 @@ namespace Bookkeeper_API.Model
             }
 
             return _instance;
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            _observers.Remove(observer);
         }
     }
 }
