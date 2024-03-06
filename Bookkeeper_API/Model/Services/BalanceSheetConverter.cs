5ca746a32819d429afcaa8501cac18a588116680
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-03-06 23:28:19 +0100

    add service to convert runtime balance sheets to dtos

---
+++ Bookkeeper_API/Model/Services/BalanceSheetConverter.cs
@@ -0,0 +1,24 @@
+﻿using Bookkeeper_API.Data.DTOs;
+
+namespace Bookkeeper_API.Model.Services
+{
+    public class BalanceSheetConverter
+    {
+        public static BalanceSheetDto BalanceSheetToBalanceSheetDto(BalanceSheet balanceSheet)
+        {
+            Dictionary<(int, string), decimal> accountDictionary = new Dictionary<(int, string), decimal>();
+
+            foreach (Account account in balanceSheet.Accounts)
+            {
+                accountDictionary.Add((account.Id, account.AccountName), account.CalculateBalance());
+            }
+
+            return new BalanceSheetDto()
+            {
+                Accounts = accountDictionary,
+                Total = balanceSheet.Total,
+                Date = balanceSheet.Date
+            };
+        }
+    }
+}
