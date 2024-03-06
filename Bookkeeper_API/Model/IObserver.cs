9c32cc18e9daab5e95e33ed570613d5e684194a6
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-03-06 21:01:16 +0100

    add interfaces for observer implementation

---
+++ Bookkeeper_API/Model/IObserver.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookkeeper_API.Model
+{
+    /// <summary>
+    /// Interface for a simple observer of a subject, according to the observer pattern.
+    /// </summary>
+    public interface IObserver
+    {
+        /// <summary>
+        /// This method gets called by the the subjects that have been subscribed to if there have been any relevant changes.
+        /// </summary>
+        void Update();
+    }
+}
---
+++ Bookkeeper_API/Model/ISubject.cs
@@ -0,0 +1,25 @@
+﻿namespace Bookkeeper_API.Model
+{
+    /// <summary>
+    /// Interface for a simple subject of an observer, according to the observer pattern.
+    /// </summary>
+    public interface ISubject
+    {
+        /// <summary>
+        /// The method observers of the according subject can use to subscribe to relevant events.
+        /// </summary>
+        /// <param name="observer">The observer passes a reference to itself so it can be added to a list.</param>
+        void Subscribe(IObserver observer);
+
+        /// <summary>
+        /// The method observers of the according subject can use to unscubscribe from once relevant events.
+        /// </summary>
+        /// <param name="observer">The observer passes a reference to itself so it can be removed from list.</param>
+        void Unsubscribe(IObserver observer);
+
+        /// <summary>
+        /// The method that should be used to notify observers of relevant events.
+        /// </summary>
+        void NotifyObservers();
+    }
+}
